595cdba7aa3f29bc3b5d28ada4c259c3ae48990c
VictorTehYJ <victortehyj@gmail.com>
2020-05-16 16:32:51 +0800

    user are able to load general channel by default

--- app.py
+++ app.py
@@ -1,22 +1,77 @@
 import os
 
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-channels={}  
-channels['General']=[] 
+channels = {}
+channels["General"] = []
 # list of all channels except General
-channelsList=[]
+channelsList = []
+privateMessages = {}
+usersList = {}
+limit = 100
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
-@socketio.on("add channel")
-def add_channel(data):
-    channel_name = data["channel_name"]
-    emit("update channel", {"channel_name": channel_name}, broadcast=True)
+@socketio.on('connect')
+def connect():
+    emit("load channels",{'channels':channels})
+    
+
+@socketio.on('new username')
+def new_username(data):
+    username=""
+    error=""
+    if data['username'] in usersList:
+        error="Username already exist. Try again"
+    else:
+        usersList[data['username']]=request.sid
+        username=data["username"]
+    emit("add username",{"username":username,'error':error})
+
+@socketio.on('new channel')
+def new_channel(data):
+    error=""
+    if data["channel"] in channelsList or data['channel']=="General":
+        error="Channel already exist. Try again."
+    elif data["channel"][0].isdigit():
+        error="Channel name cannot start with a number"   
+    elif ' ' in data['channel']:
+        error="Channel name can't contain space"  
+    else:
+        channelsList.append(data['channel'])
+        #create place for future messages
+        channels[data["channel"]]=[]
+    emit("add channel",{'channel':data["channel"],'error':error})
+
+@socketio.on('leave')
+def leave(data):
+    room = data["channel"]
+    leave_room(room)
+    message={'text':data["mymessage"],'username':data['username'],"time":data['time']}
+    channels[data["channel"]].append(message)
+    if (len(channels[data["channel"]])>limit):
+        channels[data["channel"]].pop(0)
+    emit("left",{'channels':channels},room=room)
+
+@socketio.on('join')
+def join(data):
+    room = data["channel"]
+    join_room(room)
+    message={'text':data["mymessage"],'username':data['username'],"time":data['time']}
+    channels[data["channel"]].append(message)
+    if (len(channels[data["channel"]])>limit):
+        channels[data["channel"]].pop(0)
+    emit("joined",{'channels':channels},room=room)
+
+@socketio.on('come back to general')
+def come_back_to_general():
+    emit("announce to all",{'channels':channels},broadcast=True)
+
--- static/index.js
+++ static/index.js
@@ -1,33 +1,142 @@
 $(function () {
     // document ready 
-    if (!localStorage.getItem('username'))
-        $('#myModal').modal('show')
-    else
-        $('#welcome_header').text("Hi " + localStorage.getItem('username') + ", Welcome to Flack");
-
-    $('#myModal').on('hidden.bs.modal', function () {
-        if (!localStorage.getItem('username'))
-            $('#myModal').modal('show')
-    })
-    $("#modalInput").on('keyup', function (key) {
-        if ($(this).val().length > 0) {
-            $("#modalButton").attr('disabled', false);
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+    // privateWindow = false;
+    // inRoom = false;
+    socket.on('connect', () => {
+        // prompt for display name for first time user
+        if (!localStorage.getItem("username")) {
+            $("#myModal").modal({ backdrop: 'static', keyboard: false });
         } else {
-            $("#modalButton").attr('disabled', true);
+            $("#welcome_header").text("Hi " + localStorage.getItem("username") + ", Welcome to Flack");
+            $("#myModal").modal("hide")
         }
-    });
-    $("#modalInput").on('keydown', function (key) {
-        if ($(this).val().length > 0 && key.keyCode == 13) {
-            $('#modalButton').click();
-        }
-    });
-    $("#modalButton").on('click', function () {
-        var username = $('#modalInput').val();
-        username = username.charAt(0).toUpperCase() + username.slice(1);
-        localStorage.setItem('username', username);
-        $('#welcome_header').text("Hi " + username + ", Welcome to Flack");
-        $('#myModal').modal('hide')
+        $("#myModal").on("hidden.bs.modal", function () {
+            if (!localStorage.getItem("username")) {
+                $("#myModal").modal({ backdrop: 'static', keyboard: false });
+            }
+        })
+        // modal Input check
+        $("#modalInput").on('keyup', function (key) {
+            if ($(this).val().length > 0) {
+                $("#modalButton").attr('disabled', false);
+            }
+            else {
+                $("#modalButton").attr('disabled', true);
+            }
+        });
+        $("#modalInput").on('keydown', function (key) {
+            if ($(this).val().length > 0) {
+                $("#modalButton").attr('disabled', false);
+                if (key.keyCode == 13) {
+                    $('#modalButton').click();
+                }
+            }
+        });
+        // Request to add username (send to server)
+        $("#modalButton").on("click", function () {
+            var username = $("#modalInput").val();
+            username = username.charAt(0).toUpperCase() + username.slice(1);
+            socket.emit('new username', { 'username': username });
+
+        });
+        // Request to join channel
+        $('#channelList').on('click', 'li', function () {
+            $('#messageInput').focus();
+            if (!localStorage.getItem('activeChannel')) {
+                activeChannel = "General";
+            } else {
+                activeChannel = localStorage.getItem('activeChannel');
+            }
+            const username = localStorage.getItem('username');
+            const time = new Date().toLocaleString();
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
+            $('#messages').html("");
+            if (activeChannel != "General" && !privateWindow) {
+                socket.emit('leave', { 'channel': activeChannel, 'mymessage': 'has left the room', 'username': username, 'time': time });
+            }
+            activeChannel = $("#channelList .active").attr('id');
+            localStorage.setItem('activeChannel', activeChannel)
+            if (activeChannel == 'General') {
+                inRoom = false;
+                privateWindow = false;
+                return socket.emit('come back to general');
+            } else {
+                inRoom = true;
+                privateWindow = false;
+            }
+            socket.emit('join', { 'channel': activeChannel, 'mymessage': 'has entered the room', 'username': username, 'time': time });
+        });
+        // Request to Add channel
+        $(".add_channel_btn").on('click', function () {
+            var channelName = $('.add_channel').val();
+            channelName = channelName.charAt(0).toUpperCase() + channelName.slice(1);
+            socket.emit('new channel', { 'channel': channelName });
+
+        });
+    })
+    // Load channel
+    socket.on('load channels', data => {
+        $('#channelList li').remove();
+        loadChannels(data);
+        $('#' + localStorage.getItem('activeChannel')).click();
     });
 
-
-});
+    // Respond to add username (receive from server)
+    socket.on('add username', data => {
+        if (data["error"] != "") {
+            window.setTimeout(function () {
+                $("#myModal").modal({ backdrop: 'static', keyboard: false });
+                $('.modal-title').text(data["error"]);
+                $('#modalInput').val("");
+                $("#modalButton").attr('disabled', true);
+            }, 900);
+        } else {
+            localStorage.setItem('username', data["username"]);
+            $('#username').text(localStorage.getItem('username'));
+            $("#welcome_header").text("Hi " + localStorage.getItem('username') + ", Welcome to Flack");
+            $("#myModal").modal("hide")
+            $('#General').click();
+            $('#messageInput').focus();
+        }
+    });
+    // Respond to add channel
+    socket.on('add channel', data => {
+        if (data["error"] != "") {
+            window.setTimeout(function () {
+                $('.channel_error').text(data["error"]);
+                $('.add_channel').val("");
+            }, 900);
+        } else {
+            appendChannel(data['channel']);
+            $('#channelList li:last').addClass('active');
+            $('#channelList li:last').click();
+            inRoom = true;
+            var removeHash = $('#channelList li:last').text().slice(1);
+            localStorage.setItem('activeChannel', removeHash);
+            $('#channelList').scrollTop(500000);
+            $('#messageInput').focus();
+            socket.emit('update users channels', { 'channel': data['channel'] });
+        }
+    });
+})
+function loadChannels(data) {
+    for (channel in data['channels']) {
+        appendChannel(channel);
+    }
+}
+function appendChannel(channel) {
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const div2 = document.createElement('div2');
+    const span = document.createElement('span');
+    div.className = 'd-flex bd-highlight'
+    div2.className = 'user_info'
+    li.append(div);
+    div.append(div2);
+    div2.append(span);
+    span.innerHTML = '#' + channel.charAt(0).toUpperCase() + channel.slice(1);
+    li.setAttribute("id", channel);
+    $('#channelList').append(li);
+}
